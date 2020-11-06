04e2b4dd09e7ae577be92a84614090d0316d7d57
Yeckli Lien <yeckli.lien@traveloka.com>
2020-11-06 11:00:24 +0700

    Add converter unit test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/google/go-cmp v0.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/rpc v1.2.0
 	google.golang.org/grpc v1.33.1
--- impl/converter_test.go
+++ impl/converter_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"testing"
 
 	"github.com/yecklilien/OMDB/movie"
@@ -33,11 +34,130 @@ func constructExpectedGetMovieDetailResponse() *movie.GetMovieDetailResponse {
 	return result
 }
 
+func constructExpectedSearchMovieResponse() *movie.SearchMovieResponse {
+	file, _ := ioutil.ReadFile("test_file/SearchMovieResponse.json")
+	result := &movie.SearchMovieResponse{}
+	_ = json.Unmarshal([]byte(file), result)
+	return result
+}
+
+func constructSearchMovieResponseOMDB() *searchMovieResponseOMDB {
+	file, _ := ioutil.ReadFile("test_file/SearchMovieResponseOMDB.json")
+	result := &searchMovieResponseOMDB{}
+	_ = json.Unmarshal([]byte(file), result)
+	return result
+}
+
+func TestConstructSearchMovieResponse(t *testing.T) {
+	file, _ := ioutil.ReadFile("test_file/SearchMovieResponseOMDB.json")
+	searchMovieResponse,err := constructSearchMovieResponse([]byte(file))
+	if err!= nil {
+		t.Fatal("Unexpected error thrown")
+	}
+
+	sort.SliceStable(searchMovieResponse.Movies, func(i, j int) bool {
+		return searchMovieResponse.Movies[i].Title < searchMovieResponse.Movies[j].Title
+	})
+
+	expectedSearchMovieResponse := constructExpectedSearchMovieResponse()
+
+	sort.SliceStable(expectedSearchMovieResponse.Movies, func(i, j int) bool {
+		return expectedSearchMovieResponse.Movies[i].Title < expectedSearchMovieResponse.Movies[j].Title
+	})
+
+	assertEqual(t,searchMovieResponse.String(),expectedSearchMovieResponse.String(),"")
+}
+
+func TestGetSearchMovieResponse(t *testing.T) {
+	searchMovieResponseOMDB := constructSearchMovieResponseOMDB()
+	expectedSearchMovieResponse := constructExpectedSearchMovieResponse()
+	
+	sort.SliceStable(expectedSearchMovieResponse.Movies, func(i, j int) bool {
+		return expectedSearchMovieResponse.Movies[i].Title < expectedSearchMovieResponse.Movies[j].Title
+	})
+
+	actualSearchMovieResponse,err := getSearchMovieResponse(*searchMovieResponseOMDB)
+
+	sort.SliceStable(actualSearchMovieResponse.Movies, func(i, j int) bool {
+		return actualSearchMovieResponse.Movies[i].Title < actualSearchMovieResponse.Movies[j].Title
+	})
+	
+	if err != nil {
+		t.Fatal("Unexpected error when get search movie response")
+	}
+
+	assertEqual(t, expectedSearchMovieResponse.String(), actualSearchMovieResponse.String(), "" )
+}
+
+func TestGetMovie(t *testing.T) {
+	movieOMDB := constructSearchMovieResponseOMDB().Movies[0]
+	resultChan := make(chan *movie.Movie)
+	go getMovie(movieOMDB, resultChan)
+	var movie *movie.Movie
+	movie = <-resultChan
+
+	assertEqual(t, movieOMDB.ImdbID, movie.ImdbID, "")
+	assertEqual(t, movieOMDB.Poster, movie.Poster, "")
+	assertEqual(t, movieOMDB.Title, movie.Title, "")
+	assertEqual(t, movieOMDB.Type, movie.Type, "")
+	assertEqual(t, movieOMDB.Year, movie.Year, "")
+}
+
+func TestConstructGetMovieDetailResponse(t *testing.T) {
+	expectedMovieDetail := constructExpectedGetMovieDetailResponse()
+	sort.SliceStable(expectedMovieDetail.Ratings, func(i, j int) bool {
+		if expectedMovieDetail.Ratings[i].Source == expectedMovieDetail.Ratings[j].Source {
+			return expectedMovieDetail.Ratings[i].Value < expectedMovieDetail.Ratings[j].Value
+		}
+		return expectedMovieDetail.Ratings[i].Source < expectedMovieDetail.Ratings[j].Source
+	})
+
+	file, err := ioutil.ReadFile("test_file/MovieDetailOMDB.json")
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fatalf("error when construct Get Movie Detail Response: %v", err)
+	}
+
+	actualMovieDetail, err := constructGetMovieDetailResponse([]byte(file))
+
+	fmt.Println(actualMovieDetail)
+
+	if err != nil {
+		t.Fatalf("error when construct Get Movie Detail Response: %v", err)
+	}
+
+	sort.SliceStable(actualMovieDetail.Ratings, func(i, j int) bool {
+		if actualMovieDetail.Ratings[i].Source == actualMovieDetail.Ratings[j].Source {
+			return actualMovieDetail.Ratings[i].Value < actualMovieDetail.Ratings[j].Value
+		}
+		return actualMovieDetail.Ratings[i].Source < actualMovieDetail.Ratings[j].Source
+	})
+
+	assertEqual(t, expectedMovieDetail.String(), actualMovieDetail.String(), "")
+}
+
 func TestGetGetMovieDetailResponse(t *testing.T) {
-	// movieDetailOMDB := constructMovieDetailOMDB()
-	// expectedMovieDetail := constructExpectedGetMovieDetailResponse()
-	// movieDetail := getGetMovieDetailResponse(*movieDetailOMDB)
-	// assertEqual(t, expectedMovieDetail, movieDetail, "")
+	movieDetailOMDB := constructMovieDetailOMDB()
+	expectedMovieDetail := constructExpectedGetMovieDetailResponse()
+
+	sort.SliceStable(expectedMovieDetail.Ratings, func(i, j int) bool {
+		if expectedMovieDetail.Ratings[i].Source == expectedMovieDetail.Ratings[j].Source {
+			return expectedMovieDetail.Ratings[i].Value < expectedMovieDetail.Ratings[j].Value
+		}
+		return expectedMovieDetail.Ratings[i].Source < expectedMovieDetail.Ratings[j].Source
+	})
+
+	movieDetail := getGetMovieDetailResponse(*movieDetailOMDB)
+
+	sort.SliceStable(movieDetail.Ratings, func(i, j int) bool {
+		if movieDetail.Ratings[i].Source == movieDetail.Ratings[j].Source {
+			return movieDetail.Ratings[i].Value < movieDetail.Ratings[j].Value
+		}
+		return movieDetail.Ratings[i].Source < movieDetail.Ratings[j].Source
+	})
+
+	assertEqual(t, expectedMovieDetail.String(), movieDetail.String(), "")
 }
 
 func TestGetRating(t *testing.T) {
